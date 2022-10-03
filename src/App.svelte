1855cd0a10036cc5e7cb5aa32a46477a19a840d3
raulmar <raulhdz40@gmail.com>
2022-10-03 17:18:13 -0500

    added new bg and profile pic

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,18 @@
 import LinkGallery from './lib/LinkGallery.svelte'
 import { AppConfig } from './utils/AppConfig.js';
 import "./app.css"
+import SkullNft from './assets/skullnft.webp'
 </script>
 
-<main class="max-w-xs py-5 mx-auto">
-  <div class="shadow-2xl w-8/12 mx-auto my-5">
-    <img class="" src={AppConfig.profile.image} alt={AppConfig.profile.alt}>
-    <h1 class="bg-white font-bold text-center p-2">{AppConfig.profile.title}</h1>
-  </div>
-  <LinkGallery />
-</main>
+<div class="contenedor">
+  <main class="max-w-xs">
+    <div class="shadow-2xl w-8/12 mx-auto my-5">
+      <img class="" src={SkullNft} alt={AppConfig.profile.alt}>
+      <h1 class="bg-white font-bold text-center p-2">{AppConfig.profile.title}</h1>
+    </div>
+    <LinkGallery />
+  </main>
+</div>
 
 <style>
 
